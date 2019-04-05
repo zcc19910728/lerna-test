efcbde7fae98a28d8c721996a5b30bed927d14b9
zcc <610485438@qq.com>
2019-04-05 22:04:11 +0800

    新增组件

---
+++ packages/admin-copy/build/precache-manifest.9ff7fd50f41f9f49c8a5ae5a633d8063.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8dd65e47a8bba28882d7",
+    "url": "https://ytstatic.hipac.cn/asset/fed-common-antd/admin-copy/0.1.0/static/js/vendor.8dd65e47.js"
+  },
+  {
+    "revision": "cf0fbece820561fb23b3",
+    "url": "https://ytstatic.hipac.cn/asset/fed-common-antd/admin-copy/0.1.0/static/js/main.cf0fbece.js"
+  },
+  {
+    "revision": "e982a74deb9063919aba",
+    "url": "https://ytstatic.hipac.cn/asset/fed-common-antd/admin-copy/0.1.0/static/js/3.e982a74d.chunk.js"
+  },
+  {
+    "revision": "ef93c1bfaaaae688a483",
+    "url": "https://ytstatic.hipac.cn/asset/fed-common-antd/admin-copy/0.1.0/static/js/0.ef93c1bf.chunk.js"
+  },
+  {
+    "revision": "e982a74deb9063919aba",
+    "url": "https://ytstatic.hipac.cn/asset/fed-common-antd/admin-copy/0.1.0/static/css/3.452da996.chunk.css"
+  },
+  {
+    "revision": "f9fa95dcee1d7998098040b1d37b8f47",
+    "url": "https://ytstatic.hipac.cn/asset/fed-common-antd/admin-copy/0.1.0/index.html"
+  }
+];
