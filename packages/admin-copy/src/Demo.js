90b0a9cc6f62b761f9e95a5ab2548c90bb2ab3b1
zcc <610485438@qq.com>
2019-04-05 21:59:40 +0800

    新增组件

--- packages/admin-copy/src/Demo.js
+++ packages/admin-copy/src/Demo.js
@@ -1,13 +1,14 @@
 // 组件demo写这里
 import React, { Component } from 'react';
-import Main from './Main';
+import Copy from './Main';
 
 class Demo extends Component {
+    callback = (e) => console.log(666);
 
     render() {
         return (
             <div style={{padding: 20}}>
-                <Main value={''} onChange={() => {}}/>    
+                <Copy callBack={this.callback} text="展示文本" copyData="复制文本" />
             </div>
         );
     }
--- packages/admin-copy/src/Main.js
+++ packages/admin-copy/src/Main.js
@@ -1,19 +1,37 @@
 // 组件写这里
-import React, { Component } from 'react';
+import React, { Component} from 'react';
+import { message } from 'antd';
 
-class Main extends Component {
+export default class Main extends Component {
 
-    static defaultProps = {
-        value: undefined,
-        onChange: (value) => {}
+    copy = () => {
+        const { callBack } = this.props;
+
+        this.refs.copyInput.select(); // 选中赋值过的input
+        let tag = document.execCommand("Copy"); // 执行复制
+
+        if(tag){
+            message.success('复制成功');
+            callBack && callBack();
+        }
     }
 
+    linkStyle = {
+        color: '#1890ff',
+        outline: 'none',
+        cursor: 'pointer',
+        marginLeft: 10,
+    }
+    
     render() {
+        const { copyData, text } = this.props;
+
         return (
-            <div>I am component</div>
+            <div className="d-ib">
+                <input ref="copyInput" type="text" style={{opacity: 0,width: 10}} defaultValue={copyData}/>
+                {text}    
+                <a style={this.linkStyle} onClick={this.copy}>复制</a>
+            </div>
         );
     }
-
 }
-
-export default Main
