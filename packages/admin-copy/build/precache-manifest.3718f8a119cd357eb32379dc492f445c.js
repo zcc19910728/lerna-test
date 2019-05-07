d376d341c7018d612d245275fbc8bf6a0c6fdcdc
zcc <610485438@qq.com>
2019-05-07 20:37:24 +0800

    依赖包更改

---
+++ packages/admin-copy/build/precache-manifest.3718f8a119cd357eb32379dc492f445c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "99cad26f7632ca1a2d1d",
+    "url": "https://ytstatic.hipac.cn/asset/fed-common-antd/admin-copy/0.1.2/static/js/vendor.99cad26f.js"
+  },
+  {
+    "revision": "482f240fe99f15a364f1",
+    "url": "https://ytstatic.hipac.cn/asset/fed-common-antd/admin-copy/0.1.2/static/js/main.482f240f.js"
+  },
+  {
+    "revision": "5a3c27589d5410f20a7f",
+    "url": "https://ytstatic.hipac.cn/asset/fed-common-antd/admin-copy/0.1.2/static/js/3.5a3c2758.chunk.js"
+  },
+  {
+    "revision": "a0e1f9fa8c912459dcbd",
+    "url": "https://ytstatic.hipac.cn/asset/fed-common-antd/admin-copy/0.1.2/static/js/0.a0e1f9fa.chunk.js"
+  },
+  {
+    "revision": "5a3c27589d5410f20a7f",
+    "url": "https://ytstatic.hipac.cn/asset/fed-common-antd/admin-copy/0.1.2/static/css/3.93d5cfde.chunk.css"
+  },
+  {
+    "revision": "51c5bfd04bc85d90eb190b1574af9f02",
+    "url": "https://ytstatic.hipac.cn/asset/fed-common-antd/admin-copy/0.1.2/index.html"
+  }
+];
--- packages/admin-copy/build/service-worker.js
+++ packages/admin-copy/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "https://ytstatic.hipac.cn/asset/fed-common-antd/admin-copy/0.1.1/precache-manifest.0aee7c2255af67815b8e21e989cfb01b.js"
+  "https://ytstatic.hipac.cn/asset/fed-common-antd/admin-copy/0.1.2/precache-manifest.3718f8a119cd357eb32379dc492f445c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("https://ytstatic.hipac.cn/asset/fed-common-antd/admin-copy/0.1.1/index.html", {
+workbox.routing.registerNavigationRoute("https://ytstatic.hipac.cn/asset/fed-common-antd/admin-copy/0.1.2/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
